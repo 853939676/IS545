2a5b20bd2c01e30e5eb9d799d2800f518a5f9572
853939676 <ruiliu.0502@gmail.com>
2021-05-07 11:27:01 -0500

    clean source

--- trim.py
+++ trim.py
@@ -1,25 +1,34 @@
+'''
+Data preprocessing for IS545 final project:
+This python reads the merged crashes.csv from openrefine and creates new rows, including:
+x(longitude), y(latitude), norm_x(normalized x), norm_y(normalized y).
+It also drops duplicate rows and rows that do not have valid geometry data.
+The resulted file is exported as clean_crashes.csv
+'''
 import pandas as pd
 
 df = pd.read_csv('crashes.csv')
-print(df.shape[0])
+print("number of rows in the orgininal csv: ", df.shape[0])
 df = df.drop_duplicates()
-print(df.shape[0])
+print("dropping duplicates: ", df.shape[0])
+# splitting the geometry into x and y
 df[['point','x','y']] = df.Geometry.str.split(expand=True)
 df['x'] = df['x'].str.replace('(', '')
 df['y'] = df['y'].str.replace(')', '')
-
-df['crash_severity'] = df['crash_severity'].str.replace('PD', 'Property Damage')
-df['city'] = df['city'].str.lower()
-df['collision_type'] = df['collision_type'].str.lower()
-df['weather'] = df['weather'].str.lower().replace(' / ', '/')
-df['light'] = df['light'].str.lower().replace('unkown', 'unknown')
-df['road_surface'] = df['road_surface'].str.lower().replace('unkown', 'unknown')
-df['traffic_control'] = df['traffic_control'].str.lower().replace('unkown', 'unknown').replace(' / ', '/')
-df.loc[(df['traffic_control'] == 'other regulatory sig')|(df['traffic_control'] == 'other reg. sign'),'traffic_control'] = 'other regulatory sign'
-
+# normalizing longitude and latitude
+df = df.dropna(subset=['x', 'y'])
+print("dropping null geometry: ", df.shape[0])
+xMax = df["x"].astype(float).max()
+xMin = df["x"].astype(float).min()
+yMax = df["y"].astype(float).max()
+yMin = df["y"].astype(float).min()
+df["norm_x"] = (df["x"].astype(float) - xMin)/xMax
+df["norm_x"] = 0-df["norm_x"]
+df["norm_y"] = (df["y"].astype(float) - yMin)/yMax
+# dropping useless columns
 df = df.drop_duplicates()
-print(df.shape[0])
 df = df.drop(['point', 'Geometry'], 1)
+print("number of rows in the output: ", df.shape[0])
 df.to_csv (r'clean_crashes.csv', index = False, header=True)
 
 
